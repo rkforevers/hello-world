75c773bb6569e1df6146d5d484b717a663c93d2f
rkforevers <37323501+rkforevers@users.noreply.github.com>
2020-09-19 12:52:21 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Hello World application
+*/
+package sampleapps;
+
+/**
+ *
+ * @author rkforevers
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(" Hello World");
+    }
+    
+}
